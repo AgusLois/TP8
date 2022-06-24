ce7408688f888b276ca6d7ade6f24df19a19be71
AgusLois <Agusslois98@gmail.com>
2022-06-24 12:44:24 -0300

    TP8-Lois

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import store from './store'
+
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
+new Vue({ 
+  store,
+  render: h => h(App),
 }).$mount('#app')
